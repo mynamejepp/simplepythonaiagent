96e18c9340ca7b449b9dcee5c1df6ea249523d00
mynamejepp <iamjep00@gmail.com>
2025-12-08 17:00:45 -0500

    Added messages variable to allow multiple message support

--- simple-ai-agent/main.py
+++ simple-ai-agent/main.py
@@ -1,25 +1,33 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
 
 def main():
     print("Hello from simple-ai-agent!")
+    
     load_dotenv()
+
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("API Key is None")
+    
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("prompt", type=str, help="User prompt")
     args = parser.parse_args()
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
+
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=args.prompt
+        model='gemini-2.5-flash', contents=messages
     )
+    
     if response.usage_metadata is None:
         raise RuntimeError("Failed API request.")
-    print(f"User prompt: {args.prompt}")
+    
+    #print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: {response.text}")
