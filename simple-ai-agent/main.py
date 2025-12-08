f960d104d26f5f4a250068730144692faad8f51e
mynamejepp <iamjep00@gmail.com>
2025-12-08 16:47:09 -0500

    Adjusted input to be user input

--- simple-ai-agent/main.py
+++ simple-ai-agent/main.py
@@ -1,20 +1,28 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 def main():
     print("Hello from simple-ai-agent!")
-
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("API Key is None")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents='Test Prompt'
+        model='gemini-2.5-flash', contents=args.prompt
     )
+    if response.usage_metadata is None:
+        raise RuntimeError("Failed API request.")
+    print(f"User prompt: {args.prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: {response.text}")
     
-
 if __name__ == "__main__":
     main()
